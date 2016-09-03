6ed2d2ffa571adee99a9c77c60a131ebb7065d3d
pberrecloth <peterberrecloth@gmail.com>
2016-09-03 23:47:57 +0100

    Main process restricted into borders & fill processes

--- icon-toolbox.sketchplugin/Contents/Sketch/flattenSelectedArtwork.js
+++ icon-toolbox.sketchplugin/Contents/Sketch/flattenSelectedArtwork.js
@@ -23,69 +23,66 @@ var FlattenSelectedArtwork = function(context) {
     // layers that will be processed separately
     var darkChannel = [];
     var lightChannel = [];
+    var outlinedLayers = [];
 
     // if something is selected loop through
     for(var i = 0; i < selection.count(); i++){
         var layer = selection[i];
 
+        // get layer properties
         var fill = layer.style().fills().firstObject();
         var fillColor = fill.color().hexValue();
-        var fillChannel = checkChannel(fillColor);
-
+        var filled = fill.isEnabled();
+        //var fillChannel = checkChannel(fillColor);
         var border = layer.style().borders().firstObject();
         if (border){
-          var borderColor = border.color().hexValue();
-          var borderChannel = checkChannel(borderColor);
-        }
-
-        if (border && fill){
-            //clone layer, remove border on clone, becomes the counter layer
-            var clonedLayer = clone(layer);
-            log(clonedLayer);
-
-            //outline original layer
-            flattenCombinedShape(layer);
-            outline(layer);
-
-            //toggleBorder(clonedLayer); // Bug: won't work, probably needs to be re-run next time
-
-            //layer and clonedLayer will be batch processed as channels
-
-        }
-
-        if (border){
+          log ('border found');
           var bordered = border.isEnabled();
-
-          if (bordered){
-            flattenCombinedShape(layer);
-            outline(layer);
-            groupLayersByChannel(borderChannel);
-          }
+          var borderColor = border.color().hexValue();
+          //var borderChannel = checkChannel(borderColor);
+        } else {
+          var bordered = false;
         }
 
-        if (fill){
-          var filled = fill.isEnabled();
-
-          if (filled){
-            groupLayersByChannel(fillChannel);
+        if (bordered && filled){
+          log('processing borderd && filled: ' + layer);
+            //outline(layer);
+            if (fillColor == borderColor){
+            //1. in resulting layerGroup, set child layers to Union
+            //2. flatten the combined shape again - results in one shape
+            }
+        } else {
+          if (bordered){
+            log('processing bordered: ' + layer);
+              var channel = checkChannel(borderColor);
+              outline(layer);
+              addLayerToChannelBatch(channel);
+          } else if (filled){
+            log('processing filled: ' + layer);
+              var channel = checkChannel(fillColor);
+              addLayerToChannelBatch(channel);
           }
-      }
-        // Count the layers just for kicks
-        processCount++;
+        }
+      // Count the layers just for kicks
+      processCount++;
   }
 
 
   // Unify dark and light channels separately
+
+  log ('darkChannel: ' + darkChannel);
+  log ('lightChannel: ' + lightChannel);
   batchProcess(darkChannel, union);
   batchProcess(lightChannel, union);
 
   // Subtract the two channels
   var bothChannels = darkChannel.concat(lightChannel); // Selects both channels
   batchProcess(bothChannels, subtract);
+  log('selection after subtract: ' + selection);
 
   // Flatten combined shape irreducibly
   batchProcess(selection, flattenCombinedShape);
-
+  log('selection after flatten: ' + selection);
 
   // Done!
   notify("Processed " + processCount + " layers");
