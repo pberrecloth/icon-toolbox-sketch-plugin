af213a91a41f8d2d415330d7252742a6226fbf6b
pberrecloth <peterberrecloth@gmail.com>
2016-09-09 10:32:34 +0100

    askForUserInput asks for a value

--- icon-toolbox.sketchplugin/Contents/Sketch/_dialogs.js
+++ icon-toolbox.sketchplugin/Contents/Sketch/_dialogs.js
@@ -5,3 +5,9 @@ function alert(title, message) {
 function notify(message) {
   doc.showMessage(message);
 }
+function askForUserInput(message, initialValue) {
+  // A simple pop-up with a message and text field. A intialValue can be passed to the field.
+  if (!initialValue){ initialValue = "" }
+  var value = doc.askForUserInput_initialValue(message, initialValue);
+  return value;
+}
