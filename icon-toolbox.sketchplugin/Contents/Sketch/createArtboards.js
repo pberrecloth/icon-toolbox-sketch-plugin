d8f478ac2fb07070683cff8a42982e1910cafcbc
pberrecloth <peterberrecloth@gmail.com>
2016-09-09 01:50:46 +0100

    'Create Artboards For Selection' action added
    Wraps dartboards around selected layers

--- icon-toolbox.sketchplugin/Contents/Sketch/createArtboards.js
+++ icon-toolbox.sketchplugin/Contents/Sketch/createArtboards.js
@@ -1,20 +1,42 @@
 // Run
 var createArtboards = function(context) {
 
-  //reference the Sketch Document
-  var sketch = context.api();
-  var doc = context.document;
-  var page = doc.currentPage();
+    //reference the Sketch Document
+    var sketch = context.api();
+    var doc = context.document;
+    var page = doc.currentPage();
 
-  // Include dependencies
-  @import 'includes.js';
+    // Include dependencies
+    @import 'includes.js';
 
-  //sets up two new artboards for now, will have dialog for size/quantity options?
-  //will also need auto-placement and sizing if artwork is selected
-  createNewArtboard(artboardX, artboardY, artboardSizeLg);
-  createNewArtboard(artboardX, artboardY + artboardSizeLg, artboardSizeSm);
+    // get what's selected
+    var selection = context.selection;
 
-  // Done!
-  //notify("Processed " + processCount + " layers");
+    // check something is selected
+    if (selection.count() == 0){
+      notify("Please select something.");
+    } else {
+      // before batch process begins
+      var processCount = 0;
 
-};
+      // if something is selected loop through
+      for(var i = 0; i < selection.count(); i++){
+          var layer = selection[i];
+
+          // Get layer properties
+          var layerFrame = layer.frame();
+          var layerWidth = layerFrame.width();
+          var layerHeight = layerFrame.height();
+          var layerX = layerFrame.x();
+          var layerY = layerFrame.y();
+
+          // Make slice around layer
+          createNewArtboard(layerX, layerY, layerWidth, layerHeight);
+
+          processCount++;
+      }
+    }
+
+    // Done!
+    notify("Sliced " + processCount + " layers");
+  };
